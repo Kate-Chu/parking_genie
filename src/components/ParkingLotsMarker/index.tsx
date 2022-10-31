1a709fbae672ad64b6f6299f997d0fa848faa7ef
KateChu <chu.shihchien@gmail.com>
2022-10-31 20:22:01 +0800

    fix: transformCoord function

--- src/components/ParkingLotsMarker/index.tsx
+++ src/components/ParkingLotsMarker/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import { LatLngExpression } from 'leaflet';
 import { ParkingLotsInfo } from '../../types';
+import transformCoord from '../../utils/transformCoord';
 // import { useAppSelector } from '../../store/hooks';
 
 type ParkingLotsMarkerProps = {
@@ -11,12 +12,14 @@ type ParkingLotsMarkerProps = {
 const ParkingLotsMarker: React.FC<ParkingLotsMarkerProps> = (props) => {
   // const ParkingLotsMarker = () => {
   const { data } = props;
-  console.log(data);
-  const location: LatLngExpression = [25.03648987, 121.5621068];
+  const position: LatLngExpression = transformCoord([
+    Number(data.tw97x),
+    Number(data.tw97y),
+  ]);
 
   return (
-    <Marker position={location}>
-      <Popup>Your current location</Popup>
+    <Marker position={position}>
+      <Popup>{data.name}</Popup>
     </Marker>
   );
 };
--- src/utils/transformCoord.ts
+++ src/utils/transformCoord.ts
@@ -1,12 +1,13 @@
-const proj4 = require('proj4');
+import proj4 from 'proj4';
+import { LatLngExpression } from 'leaflet';
 
 proj4.defs([
   [
-    'EPSG:4326',
+    'EPSG:4326', // 4326 = 經緯度 = wgs84
     '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees',
   ],
   [
-    'EPSG:3826',
+    'EPSG:3826', // 3826 = TWD97
     '+title=TWD97 TM2+proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=GRS80 +units=公尺 +no_defs',
   ],
   [
@@ -15,8 +16,9 @@ proj4.defs([
   ],
 ]);
 
-const transformCoord = ([x, y]: [number, number]) => {
-  return proj4(proj4('EPSG:3826'), proj4('EPSG:4326'), [x, y]); // 3826 轉 4326 (TWD97 轉經緯度)
+const transformCoord = ([x, y]: [number, number]): LatLngExpression => {
+  const [lng, lat] = proj4('EPSG:3826', 'EPSG:4326', [x, y]);
+  return [lat, lng];
 };
 
 export default transformCoord;
