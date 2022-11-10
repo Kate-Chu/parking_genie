229644f889eb00a35b6451204fe4643e8f708464
Kate <chu.shihchien@gmail.com>
2022-11-10 13:55:34 +0800

    feature: add icon illustration on map

---
+++ src/components/IconIllustration/index.tsx
@@ -0,0 +1,33 @@
+import purpleIcon from '../../assets/location-purple.png';
+import redIcon from '../../assets/location-red.png';
+import yellowIcon from '../../assets/location-yellow.png';
+import blueIcon from '../../assets/location-blue.png';
+
+const iconList = [
+  { id: 'red', space: '小於 10 位', icon: redIcon },
+  { id: 'yellow', space: '10 ~ 30 位', icon: yellowIcon },
+  { id: 'blue', space: '大於 30 位', icon: blueIcon },
+  { id: 'purple', space: '未知', icon: purpleIcon },
+];
+
+const IconIllustration = () => {
+  return (
+    <ul
+      className="absolute bottom-10 left-10 z-[9999] flex w-fit
+     flex-col gap-4 rounded-lg bg-white p-4 shadow-md"
+    >
+      {iconList.map((item) => {
+        return (
+          <li className="flex items-center">
+            <span className="mr-2">
+              <img src={item.icon} alt="space icon" className="h-6" />
+            </span>
+            <span>{item.space}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default IconIllustration;
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,12 +1,14 @@
 import Map from '../../components/Map';
 import SearchForm from '../../components/SearchForm';
 import AboutButton from '../../components/AboutButton';
+import IconIllustration from '../../components/IconIllustration';
 
 const Homepage = () => {
   return (
     <>
       <SearchForm />
       <Map />
+      <IconIllustration />
       <AboutButton />
     </>
   );
