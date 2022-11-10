cc639588e39c729653d54c3d1f871aebbdec00e8
Kate <chu.shihchien@gmail.com>
2022-11-10 20:57:40 +0800

    test: fix the problem of test react-leaflet file

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
 module.exports = {
+  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   coverageDirectory: 'coverage',
@@ -15,5 +16,18 @@ module.exports = {
       },
     ],
   },
-  setupFilesAfterEnv: ['./src/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
+  moduleNameMapper: {
+    'src/(.*)': './src/$1',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/mocks/fileMock.js',
+    '\\.(css|less|scss)$': '<rootDir>/mocks/styleMock.js',
+    '/^components/(.*)$/': '<rootDir>/src/components/$1',
+    'react-leaflet': '<rootDir>/mocks/reactLeafletMock.js',
+  },
+  moduleFileExtensions: ['js', 'ts', 'jsx', 'tsx'],
+  transformIgnorePatterns: ['/node_modules/(?!(somePkg)|react-leaflet|@react-leaflet)'],
+  roots: ['src'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  modulePaths: ['<rootDir>/src'],
 };
