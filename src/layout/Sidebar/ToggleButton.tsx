c3d1fba341e11588e254c671d80c7e14ea09bce8
Kate <chu.shihchien@gmail.com>
2022-11-11 13:14:23 +0800

    style: adjust toggle sidebar button

--- src/layout/Sidebar/ToggleButton.tsx
+++ src/layout/Sidebar/ToggleButton.tsx
@@ -9,10 +9,10 @@ const ToggleButton: React.FC<ToggleButtonProps> = (props) => {
   const { atClick } = props;
   return (
     <button
-      className="fixed bottom-[50%] left-0 z-[9999] flex h-16 w-12 items-center justify-center rounded bg-white hover:bg-light-100"
+      className="fixed bottom-[50%] left-0 z-[9999] flex h-16 w-12 items-center justify-center rounded border-t-2 border-r-2 border-b-2 border-t-light-100 border-r-light-100 border-b-light-100 bg-white hover:bg-light-100"
       onClick={atClick}
     >
-      <ToggleIcon className=" h-12 w-8 rotate-[270deg]" fill="#d8d8d8" />
+      <ToggleIcon className=" h-12 w-8 rotate-[270deg]" fill="#b5b5be" />
     </button>
   );
 };
