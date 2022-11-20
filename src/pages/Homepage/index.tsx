e17197235990b12f14e4e702d37504a46b39d4bb
Kate <chu.shihchien@gmail.com>
2022-11-20 15:38:53 +0800

    refactory: move map-related part into homepage

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,14 +1,20 @@
 import { useEffect } from 'react';
-import { MapContainer } from 'react-leaflet';
-import { LINE_TAIWAN } from '../../data/data';
-import Map from '../../components/Map';
-import SearchForm from '../../components/SearchForm';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { ToastContainer } from 'react-toastify';
 import Sidebar from '../../layout/Sidebar';
+import SearchForm from '../../components/SearchForm';
+import LocationButton from '../../components/LocationButton';
+import UserLocationMarker from '../../components/UserLocationMarker';
+import DestinationMarker from '../../components/DestinationMarker';
+import ParkingLotsMarkerContainer from '../../components/ParkingLotsMarkerContainer';
 import AboutButton from '../../components/AboutButton';
-import ToggleButton from '../../layout/Sidebar/ToggleButton';
+import ToggleButton from '../../components/ToggleButton';
+import FilterButton from '../../components/FilterButton';
 import { useAppSelector, useAppDispatch } from '../../store';
 import { userActions } from '../../store/userSlice';
+import { LINE_TAIWAN } from '../../data/data';
 import {
+  fetchParkingLotsInfo,
   fetchAvailableSpacesInfo,
   parkingLotsActions,
 } from '../../store/parkingLotsSlice';
@@ -19,6 +25,10 @@ const Homepage = () => {
   const showSidebar = useAppSelector((state) => state.user.userState.showSidebar);
   const mapBounds = useAppSelector((state) => state.parkingLots.mapBounds);
   const parkingLotsInfo = useAppSelector((state) => state.parkingLots.parkingLotsInfo);
+
+  useEffect(() => {
+    dispatch(fetchParkingLotsInfo());
+  }, [dispatch]);
 
   dispatch(fetchAvailableSpacesInfo());
 
@@ -34,12 +44,30 @@ const Homepage = () => {
     dispatch(userActions.toggleSidebar());
   };
 
+  const toggleHideUnknownSpacesLots = () => {
+    console.log('clicked');
+    dispatch(parkingLotsActions.toggleHideUnknownSpacesLots());
+  };
+
   return (
-    <MapContainer center={LINE_TAIWAN} zoom={17} scrollWheelZoom>
-      <SearchForm />
-      <ToggleButton atClick={toggleSidebarHandler} />
+    <MapContainer center={LINE_TAIWAN} zoom={17} scrollWheelZoom doubleClickZoom={false}>
+      <section className="flex">
+        <SearchForm />
+        <FilterButton text="顯示未知車位" onClick={toggleHideUnknownSpacesLots} />
+      </section>
+      <ToggleButton atClick={toggleSidebarHandler} showSidebar={showSidebar} />
       {showSidebar && <Sidebar />}
-      <Map />
+      <div id="map" data-testid="map">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <ToastContainer />
+        <LocationButton />
+        <UserLocationMarker />
+        <DestinationMarker />
+        <ParkingLotsMarkerContainer />
+      </div>
       <AboutButton />
     </MapContainer>
   );
